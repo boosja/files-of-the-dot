bf84206219173fec10115649145108e30b3d4495
boosja <mathias.iversen487@gmail.com>
2025-05-18 21:39:07 +0200

    qutebrowser: remember session tabs

---
+++ qutebrowser/.config/qutebrowser/config.py
@@ -0,0 +1,9 @@
+# Documentation:
+#   qute://help/configuring.html
+#   qute://help/settings.html
+
+# Change the argument to True to still load settings configured via autoconfig.yml
+config.load_autoconfig(False)
+
+# Autosave session
+config.set('auto_save.session', True)
